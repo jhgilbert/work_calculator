53e099f82b55726d7f7f9f231facfa5fc00ba814
jhgilbert <j.h.gilbert@gmail.com>
2012-11-29 17:26:36 -0800

    Add countdown behavior to Sprint class file

--- sprint.rb
+++ sprint.rb
@@ -1,12 +1,19 @@
 class Sprint
+  attr_accessor :starting_point, :length
   
-  def initialize(starting_point, length)
-    @starting_point = starting_point
-    @length = length
+  def initialize
+    # gather information about session starting point and length
   end
   
-  def countdown do
-    # countdown behavior
+  def countdown
+    sprint_seconds = sprint.length_to_i * 60
+    loop do
+      sleep(300) #needs to be 300, any deviation is just for testing
+      sprint_seconds = sprint_seconds - 300
+      sprint.length = sprint_seconds / 60
+      system("say '#{sprint.length} minutes remaining.'")
+      break if sprint_seconds == 0
+    end
   end
   
-end
+end 
