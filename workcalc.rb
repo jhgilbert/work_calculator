d6d3dc055839f0e8e986ce5b79e694f37b193276
jhgilbert <j.h.gilbert@gmail.com>
2012-11-29 18:13:49 -0800

    Fix error in sprint loop

--- workcalc.rb
+++ workcalc.rb
@@ -1,17 +1,11 @@
 include Enumerable
 
-class Array
-  def sum
-    self.inject{|sum,x| sum + x }
-  end
-end
-
 profits_array = []
 session_lengths = []
 
 loop do
   puts "Would you like to do a sprint? Y/N"
-  answer = gets.to_s
+  answer = gets.chomp
   if answer == "Y" do
     sprint = Sprint.new
     sprint.start # Asks about current spot in manuscript, length of desired sprint, and starts the clock
@@ -28,37 +22,37 @@ end
 
 
 OLD CODE--------------------------------------------
-include Enumerable
+# include Enumerable
 
-class Array
-  def sum
-    self.inject{|sum,x| sum + x }
-  end
-end
+# class Array
+  # def sum
+    # self.inject{|sum,x| sum + x }
+    # end
+# end
 
-work_summary = []
-minutes_worked = []
-t = Time.new
+# work_summary = []
+# minutes_worked = []
+# t = Time.new
 
 stopping_point = nil
 
 
 
   
-  system('say "Time is up."')
+#  system('say "Time is up."')
   
-  puts "Time is up. Where are you now?"
-  current_place = gets.to_f
+#  puts "Time is up. Where are you now?"
+#  current_place = gets.to_f
 
-  words_edited = current_place - starting_point
+#  words_edited = current_place - starting_point
   
-  stopping_point = current_place.to_i
+#  stopping_point = current_place.to_i
 
-  profit = (words_edited * 0.0104) * 0.66 # value in parens is 0.0104 for most projects, .00324 for QRs
-  puts "You have made $#{"%.2f" % profit} in #{sprint_minutes} minutes."
+#  profit = (words_edited * 0.0104) * 0.66 # value in parens is 0.0104 for most projects, .00324 for QRs
+#  puts "You have made $#{"%.2f" % profit} in #{sprint_minutes} minutes."
   
-  work_summary << "$#{"%.2f" % profit}"
-  minutes_worked << sprint_minutes
+#  work_summary << "$#{"%.2f" % profit}"
+#  minutes_worked << sprint_minutes
   
   puts "*******"
   puts "Profits by session:"
