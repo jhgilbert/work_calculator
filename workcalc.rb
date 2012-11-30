2c47b498cfc8952c5611dd387951cd3fc7ed0096
jhgilbert <j.h.gilbert@gmail.com>
2012-11-29 17:11:23 -0800

    Start refactoring program into classes

--- workcalc.rb
+++ workcalc.rb
@@ -26,15 +26,15 @@ loop do
   
   sleep(sprint_seconds)
   
-  # loop do
+  loop do
   
-  # sleep(300) #needs to be 300, any deviation is just for testing
-  # sprint_seconds = sprint_seconds - 300
-  # minutes_remaining = sprint_seconds / 60
-  # system("say '#{minutes_remaining} minutes remaining.'")
+  sleep(300) #needs to be 300, any deviation is just for testing
+  sprint_seconds = sprint_seconds - 300
+  minutes_remaining = sprint_seconds / 60
+  system("say '#{minutes_remaining} minutes remaining.'")
   
-  # break if sprint_seconds == 0
-  # end
+  break if sprint_seconds == 0
+  end
   
   system('say "Time is up."')
   
