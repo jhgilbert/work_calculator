18154c04b84aad993b9ecf3bf775291b684faf2a
jhgilbert <j.h.gilbert@gmail.com>
2012-11-29 17:59:58 -0800

    Add Sprint#debrief behavior

--- sprint.rb
+++ sprint.rb
@@ -1,6 +1,6 @@
 class Sprint
   attr_accessor :begin_word_count, :length, :end_word_count, :profit, 
-  :time_remaining
+  :time_remaining, :words_edited
   
   def start
     puts "What is your starting point?"
@@ -19,5 +19,14 @@ class Sprint
       system("say '#{@time_remaining / 60} minutes remaining.'")
       break if @time_remaining == 0
     end
+  end
+  
+  def debrief
+    puts "Time is up. Where are you now?"
+    @end_word_count = gets.to_f
+    @words_edited = @end_word_count - @begin_word_count
+    @profit = (@words_edited * 0.0104) * 0.66
+    @profit = "%.2f" % @profit
+    puts "You have made $#{@profit} in #{@length} minutes."
   end
   
