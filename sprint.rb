b733d1ab82dd0ebc03845d2468c526cae22ab918
jhgilbert <j.h.gilbert@gmail.com>
2012-11-29 17:14:17 -0800

    Add Sprint class file

---
+++ sprint.rb
@@ -0,0 +1,12 @@
+class Sprint
+  
+  def initialize(starting_point, length)
+    @starting_point = starting_point
+    @length = length
+  end
+  
+  def countdown do
+    # countdown behavior
+  end
+  
+end
